a074bd122a5edd1254aaa0d5776faeda49f54415
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2021-03-05 20:25:13 +0000

    Update init.py

--- init.py
+++ init.py
@@ -1,11 +1,8 @@
 # --------------------------------------------------------------
 #  init.py
 #  Version: 1.0.10
-#  Last Updated: Jun 18th, 2020
+#  Last Updated: March 5th, 2021
 # --------------------------------------------------------------
 
 nuke.pluginAddPath('./gizmos')
 nuke.pluginAddPath('./icons')
-
-Expression_path = "/mnt/home/attila.gasparetz/.nuke/Expression"
-nuke.pluginAddPath(Expression_path)
