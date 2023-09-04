b4367c79968f43e6c3eea1de63e135d068507879
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2023-09-04 21:32:49 +0100

    Update disconnect_viewers.py

--- python/disconnect_viewers.py
+++ python/disconnect_viewers.py
@@ -1,15 +1,1 @@
-import nuke
 
-# From http://www.lookinvfx.com/nuke-python-snippets/
-
-def disconnectViewers():
-    nuke.selectAll()
-    nuke.invertSelection()
-
-    for n in nuke.allNodes():
-        if n.Class() == "Viewer":
-            n['selected'].setValue(True)
-
-    nuke.extractSelected()
-
-nuke.addOnScriptLoad(disconnectViewers)
