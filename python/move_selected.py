7eb247c2b259bcbaa236c02d17f23bae729181c5
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2023-09-21 15:55:37 +0100

    Update move_selected.py

--- python/move_selected.py
+++ python/move_selected.py
@@ -3,51 +3,35 @@ import nuke
 ### Move 1
 
 def move_up_one():
-    grid_height = nuke.toNode('preferences')['GridHeight'].value()
-    sel = nuke.selectedNodes()
-    for n in sel:
-        n['ypos'].setValue(int(n['ypos'].value()) - int(grid_height))
+    for n in nuke.selectedNodes():
+        n['ypos'].setValue(int(n['ypos'].value()) - int(nuke.toNode('preferences')['GridHeight'].value()))
 
 def move_down_one():
-    grid_height = nuke.toNode('preferences')['GridHeight'].value()
-    sel = nuke.selectedNodes()
-    for n in sel:
-        n['ypos'].setValue(int(n['ypos'].value()) + int(grid_height))
+    for n in nuke.selectedNodes():
+        n['ypos'].setValue(int(n['ypos'].value()) + int(nuke.toNode('preferences')['GridHeight'].value()))
 
 def move_left_one():
-    grid_width = nuke.toNode('preferences')['GridWidth'].value()
-    sel = nuke.selectedNodes()
-    for n in sel:
-        n['xpos'].setValue(int(n['xpos'].value()) - int(grid_width))
+    for n in nuke.selectedNodes():
+        n['xpos'].setValue(int(n['xpos'].value()) - int(nuke.toNode('preferences')['GridWidth'].value()))
 
 def move_right_one():
-    grid_width = nuke.toNode('preferences')['GridWidth'].value()
-    sel = nuke.selectedNodes()
-    for n in sel:
-        n['xpos'].setValue(int(n['xpos'].value()) + int(grid_width))
+    for n in nuke.selectedNodes():
+        n['xpos'].setValue(int(n['xpos'].value()) + int(nuke.toNode('preferences')['GridWidth'].value()))
 
 ### Move 10
 
 def move_up_ten():
-    grid_height = nuke.toNode('preferences')['GridHeight'].value()
-    sel = nuke.selectedNodes()
-    for n in sel:
-        n['ypos'].setValue(int(n['ypos'].value()) - int(grid_height*10))
+    for n in nuke.selectedNodes():
+        n['ypos'].setValue(int(n['ypos'].value()) - int(nuke.toNode('preferences')['GridHeight'].value()*10))
 
 def move_down_ten():
-    grid_height = nuke.toNode('preferences')['GridHeight'].value()
-    sel = nuke.selectedNodes()
-    for n in sel:
-        n['ypos'].setValue(int(n['ypos'].value()) + int(grid_height*10))
+    for n in nuke.selectedNodes():
+        n['ypos'].setValue(int(n['ypos'].value()) + int(nuke.toNode('preferences')['GridHeight'].value()*10))
 
 def move_left_ten():
-    grid_width = nuke.toNode('preferences')['GridWidth'].value()
-    sel = nuke.selectedNodes()
-    for n in sel:
-        n['xpos'].setValue(int(n['xpos'].value()) - int(grid_width*10))
+    for n in nuke.selectedNodes():
+        n['xpos'].setValue(int(n['xpos'].value()) - int(nuke.toNode('preferences')['GridWidth'].value()*10))
 
 def move_right_ten():
-    grid_width = nuke.toNode('preferences')['GridWidth'].value()
-    sel = nuke.selectedNodes()
-    for n in sel:
-        n['xpos'].setValue(int(n['xpos'].value()) + int(grid_width*10))
+    for n in nuke.selectedNodes():
+        n['xpos'].setValue(int(n['xpos'].value()) + int(nuke.toNode('preferences')['GridWidth'].value()*10))
