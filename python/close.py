d6e9a70699cbaf75fd27adef96a14d4642a6c2a2
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2023-09-26 11:35:26 +0100

    Update close.py

--- python/close.py
+++ python/close.py
@@ -1,6 +1,12 @@
-import nuke 
+# --------------------------------------------------------------
+#  close.py
+#  Last Updated by: Attila Gasparetz
+#  Last Updated: 26/09/2023
+# --------------------------------------------------------------
 
 # From https://www.ftrack.com/en/2019/09/8-ways-to-increase-your-efficiency-with-foundrys-nuke.html
+
+import nuke 
 
 def close():
     for node in nuke.allNodes(recurseGroups=True):
