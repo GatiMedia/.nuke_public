a08f2fe92bff56124f4fea5d64eb06728884699b
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2023-10-16 14:55:01 +0100

    Update layer_shuffle.py

--- python/layer_shuffle.py
+++ python/layer_shuffle.py
@@ -15,62 +15,59 @@ def layerShuffle():
     if not len(nodes) == 1:
         nuke.alert('<font color=orange><h3>Please, select a single Read node first!')
     else:
-        if not nodes[0].Class() == "Read":
-            nuke.alert('<font color=orange><h3>Please, select a single Read node first!')
+        curSel = nuke.selectedNode()
+        curSelChannels = curSel.channels()
+        layersList = []
+        for rawChannels in curSelChannels:
+            splitRawChannels = rawChannels.split('.')
+            layersList.append(splitRawChannels[0])
+        channelLayers = list(set(layersList))
+        channelLayers.sort()
+        noteVal = '' + '\n'.join(channelLayers)
+
+        if len(channelLayers) == 1:
+            nuke.alert('<font color=orange><h3><center>Not enough layers to break it down!\n\nOnly found:\n\n<font color=yellow><h4>' + ',\n'.join(channelLayers))
         else:
-            curSel = nuke.selectedNode()
-            curSelChannels = curSel.channels()
-            layersList = []
-            for rawChannels in curSelChannels:
-                splitRawChannels = rawChannels.split('.')
-                layersList.append(splitRawChannels[0])
-            channelLayers = list(set(layersList))
-            channelLayers.sort()
-            noteVal = '' + '\n'.join(channelLayers)
+            # create dotMain
+            dot = connectDot = nuke.nodes.Dot()
+            dot['xpos'].setValue(int(curSel['xpos'].value()) + (curSel.screenWidth() / 2) - (dot.screenWidth() / 2))
+            dot['ypos'].setValue(int(curSel['ypos'].value()) + 300)
+            dot.setSelected(SELECT_VAL)
+            dot.setInput(0, curSel)
 
-            if len(channelLayers) == 1:
-                nuke.alert('<font color=orange><h3><center>Not enough layers to break it down!\n\nOnly found:\n\n<font color=yellow><h4>' + ',\n'.join(channelLayers))
-            else:
-                # create dotMain
-                dot = connectDot = nuke.nodes.Dot()
-                dot['xpos'].setValue(int(curSel['xpos'].value()) + (curSel.screenWidth() / 2) - (dot.screenWidth() / 2))
-                dot['ypos'].setValue(int(curSel['ypos'].value()) + 300)
+            # creating layerContact
+            layerContact = nuke.nodes.LayerContactSheet()
+            layerContact['xpos'].setValue(int(dot['xpos'].value())-36)
+            layerContact['ypos'].setValue(int(dot['ypos'].value()) + Y_DIST)
+            layerContact['showLayerNames'].setValue(True)
+            layerContact.setSelected(SELECT_VAL)
+            layerContact.setInput(0, dot)
+
+            for index, n in enumerate(channelLayers):
+                # creating dot
+                newX = dot['xpos'].value()
+                newY = dot['ypos'].value()
+                oldDot = dot
+                dot = nuke.nodes.Dot()
+                dot['xpos'].setValue(int(newX) + X_DIST)
+                dot['ypos'].setValue(newY)
                 dot.setSelected(SELECT_VAL)
-                dot.setInput(0, curSel)
+                newDot = dot
+                newDot.setInput(0, oldDot)
 
-                # creating layerContact
-                layerContact = nuke.nodes.LayerContactSheet()
-                layerContact['xpos'].setValue(int(dot['xpos'].value())-36)
-                layerContact['ypos'].setValue(int(dot['ypos'].value()) + Y_DIST)
-                layerContact['showLayerNames'].setValue(True)
-                layerContact.setSelected(SELECT_VAL)
-                layerContact.setInput(0, dot)
+                # creating shuffle
+                shuffle = nuke.nodes.Shuffle2()
+                shuffle['in1'].setValue(n)
+                shuffle['label'].setValue("<b><center>" + n + "</b></center>")
+                shuffle['note_font_size'].setValue(22)
+                shuffle['xpos'].setValue(int(dot['xpos'].value())-36)
+                shuffle['ypos'].setValue(int(dot['ypos'].value()) + Y_DIST)
+                shuffle.setSelected(SELECT_VAL)
+                shuffle.setInput(0, newDot)
 
-                for index, n in enumerate(channelLayers):
-                    # creating dot
-                    newX = dot['xpos'].value()
-                    newY = dot['ypos'].value()
-                    oldDot = dot
-                    dot = nuke.nodes.Dot()
-                    dot['xpos'].setValue(int(newX) + X_DIST)
-                    dot['ypos'].setValue(newY)
-                    dot.setSelected(SELECT_VAL)
-                    newDot = dot
-                    newDot.setInput(0, oldDot)
-
-                    # creating shuffle
-                    shuffle = nuke.nodes.Shuffle2()
-                    shuffle['in1'].setValue(n)
-                    shuffle['label'].setValue("<b><center>" + n + "</b></center>")
-                    shuffle['note_font_size'].setValue(22)
-                    shuffle['xpos'].setValue(int(dot['xpos'].value())-36)
-                    shuffle['ypos'].setValue(int(dot['ypos'].value()) + Y_DIST)
-                    shuffle.setSelected(SELECT_VAL)
-                    shuffle.setInput(0, newDot)
-
-                # creating stickyNote
-                stickyNote = nuke.nodes.StickyNote()
-                stickyNote['xpos'].setValue(int(layerContact['xpos'].value()))
-                stickyNote['ypos'].setValue(int(layerContact['ypos'].value()) + Y_DIST)
-                stickyNote['label'].setValue("<h2><center>Available layers:\n\n" + noteVal + "\n\n")
-                stickyNote.setSelected(SELECT_VAL)
+            # creating stickyNote
+            stickyNote = nuke.nodes.StickyNote()
+            stickyNote['xpos'].setValue(int(layerContact['xpos'].value()))
+            stickyNote['ypos'].setValue(int(layerContact['ypos'].value()) + Y_DIST)
+            stickyNote['label'].setValue("<h2><center>Available layers:\n\n" + noteVal + "\n\n")
+            stickyNote.setSelected(SELECT_VAL)
