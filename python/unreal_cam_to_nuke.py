18880080da7105b5dd0e065ea86a33b2d7fcfb57
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2024-06-11 12:41:30 +0100

    Update unreal_cam_to_nuke.py

--- python/unreal_cam_to_nuke.py
+++ python/unreal_cam_to_nuke.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------
 #  unreal_cam_to_nuke.py
-#  Version: 1.0
-#  Last Updated: 07/02/2024
+#  Version: 1.1
+#  Last Updated: 11/06/2024
 #  Last updated by: Attila Gasparetz
 # --------------------------------------------------------------
 import nuke
@@ -24,4 +24,19 @@ def unrealCamToNuke():
             conv_cam['rotate'].setExpression("-parent."+sel_cam_name+".rotate.y",0)
             conv_cam['rotate'].setExpression("parent."+sel_cam_name+".rotate.z-90",1)
             conv_cam['rotate'].setExpression("-parent."+sel_cam_name+".rotate.x+90",2)
+            conv_cam['projection_mode'].setExpression("parent."+sel_cam_name+".projection_mode")
+            conv_cam['focal'].setExpression("parent."+sel_cam_name+".focal")
+            conv_cam['haperture'].setExpression("parent."+sel_cam_name+".haperture")
+            conv_cam['vaperture'].setExpression("parent."+sel_cam_name+".vaperture")
+            conv_cam['near'].setExpression("parent."+sel_cam_name+".near")
+            conv_cam['far'].setExpression("parent."+sel_cam_name+".far")
+            conv_cam['win_translate'].setExpression("parent."+sel_cam_name+".win_translate",0)
+            conv_cam['win_translate'].setExpression("parent."+sel_cam_name+".win_translate",1)
+            conv_cam['win_scale'].setExpression("parent."+sel_cam_name+".win_scale",0)
+            conv_cam['win_scale'].setExpression("parent."+sel_cam_name+".win_scale",1)
+            conv_cam['winroll'].setExpression("parent."+sel_cam_name+".winroll")
+            conv_cam['focal_point'].setExpression("parent."+sel_cam_name+".focal_point")
+            conv_cam['fstop'].setExpression("parent."+sel_cam_name+".fstop")
+            conv_cam['shutter'].setExpression("parent."+sel_cam_name+".shutter")
+            conv_cam['shutteroffset'].setExpression("parent."+sel_cam_name+".shutteroffset")
             conv_cam.setSelected(True)
